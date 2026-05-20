5b66dbdd0dc72969be5da65e826e6133cb321897
sjeletich <sjeletich@student.tudelft.nl>
2026-05-20 12:51:21 +0200

    Setup files

---
+++ src/import_data.py
@@ -0,0 +1,48 @@
+
+"""
+Code for reading in the C-MAPSS data (FD001) 
+"""
+import pandas as pd
+
+"""
+For your final RUL prediction model (part 2 of the assignment), do NOT use the engine number or cycle number as input for your model.
+The lifetimes of the engine in this simulated dataset is relatively short compared to the life time of real-life engines. 
+The cycle number therefore gives more information about the RUL than would be the case in real life.
+You do need the cycle number and engine number to calculate the RUL. 
+"""
+
+def load_data():
+    index_names = ['engine', 'cycle'] 
+
+    # Operational conditions: altitude, mach_number and throttle_resolver_angle
+    operational_condition_names = ['altitude',  'mach_nr', 'TRA']
+    sensor_names = ['T2', # total temperature at fan inlet
+                    'T24',# total temperature at LPC outlet
+                    'T30', # total temperature at HPC outlet
+                    'T50', # total temperature at LPT outlet
+                    'P2', # Pressure at fan inlet
+                    'P15', #Total pressure in bypass-duct
+                    'P30', #Total pressure at HPC outlet
+                    'Nf', #Physical fan speed rpm
+                    'Nc', #Physical core speed rpm
+                    'epr', #Engine pressure ratio (P50/P2)
+                    'Ps30', #Static pressure at HPC outlet
+                    'phi', #Ratio offuel flow to Ps30
+                    'NRf', #Corrected fan speed
+                    'NRc', #Corrected core speed
+                    'BPR', #Bypass Ratio
+                    'farB', #Burner fuel-air ratio
+                    'htBleed', #Bleed Enthalpy
+                    'Nf_dmd', # Demanded fan speed rpm
+                    'PCNfR_dmd', #Demanded corrected fan speed rpm
+                    'W31', #HPT coolant bleed lbm/s
+                    'W32', #LPT coolant bleed
+                    ]
+    # options to visualize the datadrame
+    col_names =  index_names + operational_condition_names + sensor_names
+
+    df_train = pd.read_csv(r'C:\Users\Storm\OneDrive\Documenten\GitHub\AE4465\CMAPSSData\train_FD001.txt' ,  sep = ' ' , names=col_names, index_col = False,  usecols=range(len(col_names))) 
+
+    df_test = pd.read_csv(r'C:\Users\Storm\OneDrive\Documenten\GitHub\AE4465\CMAPSSData\test_FD001.txt' , sep=' ' , names= col_names, index_col = False,  usecols=range(len(col_names)))
+
+    return df_train, df_test
